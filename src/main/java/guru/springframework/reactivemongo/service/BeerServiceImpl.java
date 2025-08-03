75d8f2a343c2348da81e17a7b7cbf9ac2fbb6da9
Lydia Rennert <lydia.rennert@gmail.com>
2025-08-03 14:27:04 +0200

    lre: Add integration test for saveBeer

--- src/main/java/guru/springframework/reactivemongo/service/BeerServiceImpl.java
+++ src/main/java/guru/springframework/reactivemongo/service/BeerServiceImpl.java
@@ -1,15 +1,24 @@
 package guru.springframework.reactivemongo.service;
 
+import guru.springframework.reactivemongo.mapper.BeerMapper;
 import guru.springframework.reactivemongo.model.BeerDTO;
+import guru.springframework.reactivemongo.repository.BeerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class BeerServiceImpl implements BeerService {
+
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
 
     @Override
     public Mono<BeerDTO> saveBeer(Mono<BeerDTO> beerDto) {
-        return null;
+        return beerDto.map(beerMapper::beerDtoToBeer)
+                .flatMap(beerRepository::save)
+                .map(beerMapper::beerToBeerDto);
     }
 
     @Override
