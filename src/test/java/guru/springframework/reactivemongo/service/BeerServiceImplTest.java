f333d8fe03a27eb8f4fbea8adf4e9395d1af4ded
Lydia Rennert <lydia.rennert@gmail.com>
2025-08-07 22:24:35 +0200

    lre: Add method findFirstByBeerName

--- src/test/java/guru/springframework/reactivemongo/service/BeerServiceImplTest.java
+++ src/test/java/guru/springframework/reactivemongo/service/BeerServiceImplTest.java
@@ -35,6 +35,21 @@ class BeerServiceImplTest {
     }
 
     @Test
+    void findFirstByBeerName() {
+        BeerDTO beerDto = getSavedBeerDto();
+
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+        Mono<BeerDTO> foundDto = beerService.findFirstByBeerName(beerDto.getBeerName());
+
+        foundDto.subscribe(dto -> {
+            System.out.println(dto.toString());
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
+    }
+
+    @Test
     @DisplayName("Test Save Beer Using Subscriber")
     void saveBeerUseSubscriber() {
 
